0bf169206c26188b8e2409e8d7218098c709329c
AhmadMunib23 <148643435+AhmadMunib23@users.noreply.github.com>
2023-10-22 11:18:34 +0700

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,14 @@
+<?php
+
+$nrp = "701220124";
+$nama = "ahmad munib";
+$umur = 18;
+$nilai = 80.80;
+
+echo "Nrp :". $nrp  . "</br>";
+echo "Nama : $nama</br>";
+print "Umur :".$Umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit dibelakang koma
+printf ("Nilai : %.3f</br>" , $nilai) ;
+
+?>
